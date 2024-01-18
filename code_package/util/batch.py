b037dd5f738ccddf5e38cd3195f84f0b78c0e4f4
Varun Mangalick <varunm22@gmail.com>
2024-01-18 15:52:37 -0500

    more bug fix

--- code_package/util/batch.py
+++ code_package/util/batch.py
@@ -3,7 +3,7 @@ from typing import Dict, List
 
 
 def submit_batch_job(command: str, job_name: str, dependencies: List[str] = []):
-    session = boto3.session.Session(profile_name="personal", region_name="us-east-1")
+    session = boto3.session.Session()
     batch = session.client("batch")
     submit_job_response = batch.submit_job(
         jobName=job_name,
@@ -18,7 +18,7 @@ def submit_batch_job(command: str, job_name: str, dependencies: List[str] = []):
 
 
 def get_job_status(job_id: str) -> Dict:
-    session = boto3.session.Session(profile_name="personal", region_name="us-east-1")
+    session = boto3.session.Session()
     batch = session.client("batch")
     describe_jobs_response = batch.describe_jobs(jobs=[job_id])
     return describe_jobs_response["jobs"][0]["status"]
