b84a2c240b1783afc6da7d2fafc7a67af1845a06
Varun Mangalick <varunm22@gmail.com>
2024-01-22 11:14:13 -0500

    .

--- code_package/countdown/countdown.py
+++ code_package/countdown/countdown.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 import time
+from code_package.util.cloud import s3_ls
 
 parser = argparse.ArgumentParser()
 parser.add_argument("seconds", type=int)
@@ -12,5 +13,7 @@ for i in range(args.seconds, 0, -1):
     print(i)
     time.sleep(1)
 print("Countdown finished")
+
+print(s3_ls(args.output_dir))
 
 pd.DataFrame({"seconds": [args.seconds]}).to_csv(args.output_dir + "/seconds.csv")
